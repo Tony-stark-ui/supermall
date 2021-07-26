58b793c74e341696ad1e05524d0ca64acbada5bf
我妻善逸 <2848787564@qq.com>
2021-07-26 15:24:10 +0800

    ‘初始化项目’

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 module.exports = {
+  // 基本路径
+  // publicPath: './',
+  // // 输出文件目录
+  // outputDir: 'dist',
+  // configureWebpack: {
+  //   externals: {
+  //   }
+  // },
   configureWebpack: {
     // 配置路径别名
     resolve: {
